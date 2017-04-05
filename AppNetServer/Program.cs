03c23b5df8a25a9e7b8eb4c380e1b1028c5c46f5
Anton Markaj <thony_m89@hotmail.com>
2017-04-05 07:51:11 +0200

    Inital Coomit

--- AppNetServer/Program.cs
+++ AppNetServer/Program.cs
@@ -9,6 +9,10 @@ namespace AppNetServer
         {
             using (WebApp.Start<Startup>("http://appnet.hsr.ch:40000"))
             {
+                Console.WriteLine("####################");
+                Console.WriteLine("#### AppNet v1.0 ###");
+                Console.WriteLine("####################");
+                Console.WriteLine("--------------------");
                 Console.WriteLine("AppNet Web Service is running...");
                 Console.WriteLine("Press any key to quit the AppNet Web Service.");
                 Console.ReadLine();
