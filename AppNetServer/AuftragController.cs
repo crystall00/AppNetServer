311b70afdb81ab4ee8195e47f373a6d784ce3a46
Anton Markaj <thony_m89@hotmail.com>
2017-04-11 14:25:13 +0200

    AuftragController ergaenzung

--- AppNetServer/AuftragController.cs
+++ AppNetServer/AuftragController.cs
@@ -10,5 +10,20 @@ namespace AppNetServer
 {
     class AuftragController : ApiController
     {
+        private AppNetServices service = new AppNetServices();
+        // GET: api/demo
+        public ArrayList Get()
+        {
+            return service.getAllOrders();
+        }
+
+        // POST: api/demo
+        public HttpResponseMessage Post([FromBody]Auftrag auftrag)
+        {
+            service.save(auftrag);
+            HttpResponseMessage response = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+            //response.Headers.Location = new Uri(Request.RequestUri, String.Format("demo"));
+            return response;
+        }
     }
 }
