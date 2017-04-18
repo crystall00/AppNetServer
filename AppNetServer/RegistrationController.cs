de1686107a85708067877ebc37c916820afee49c
Anton Markaj <thony_m89@hotmail.com>
2017-04-18 15:56:15 +0200

    registrations - controller

---
+++ AppNetServer/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using AppNetServer;
+using AppNetServer.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppNetServer
+{
+    public class RegistrationController : ApiController
+    {
+        private AppNetServices service = new AppNetServices();
+        // GET: api/auftrag
+        public ArrayList Get()
+        {
+            return service.getAllOrders();
+        }
+
+        // POST: api/auftrag
+        public HttpResponseMessage Post([FromBody]Auftrag auftrag)
+        {
+            service.save(auftrag);
+            HttpResponseMessage response = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+            //response.Headers.Location = new Uri(Request.RequestUri, String.Format("demo"));
+            return response;
+        }
+    }
+}
