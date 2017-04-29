30ea177db7f66221b9f4b262807c4fddd48ce41e
Anton Markaj <thony_m89@hotmail.com>
2017-04-29 13:50:02 +0200

    Added Controllers

---
+++ AppNetServer/ProfilController.cs
@@ -0,0 +1,35 @@
+﻿using AppNetServer;
+using AppNetServer.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppNetServer
+{
+    public class ProfilController : ApiController
+    {
+        private AppNetServices service = new AppNetServices();
+        // GET: api/auftrag/sortierParameter
+        public ArrayList Get(int sortParameter, bool published)
+        {
+            return service.getAllOrders(sortParameter, published);
+        }
+
+        // GET: api/auftrag/sortierParameter&published=false&userId
+        public ArrayList Get(int sortParameter, bool published, int userId)
+        {
+            return service.getYourOrders(sortParameter, published, userId);
+        }
+
+        // POST: api/auftrag
+        public HttpResponseMessage Post([FromBody]Auftrag auftrag)
+        {
+            service.saveOrder(auftrag);
+            HttpResponseMessage response = Request.CreateResponse(System.Net.HttpStatusCode.Created);
+            //response.Headers.Location = new Uri(Request.RequestUri, String.Format("demo"));
+            return response;
+        }
+    } 
+}
