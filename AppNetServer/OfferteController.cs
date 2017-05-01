e55bbe9c30a55130c0e994508e1ff2c90d48be04
Anton Markaj <thony_m89@hotmail.com>
2017-05-01 17:50:42 +0200

    OfferteController fixed

--- AppNetServer/OfferteController.cs
+++ AppNetServer/OfferteController.cs
@@ -26,7 +26,7 @@ namespace AppNetServer
         // POST: api/auftrag
         public HttpResponseMessage Post([FromBody]Auftrag auftrag)
         {
-            service.saveOrder(auftrag);
+            service.addAuftrag(auftrag);
             HttpResponseMessage response = Request.CreateResponse(System.Net.HttpStatusCode.Created);
             //response.Headers.Location = new Uri(Request.RequestUri, String.Format("demo"));
             return response;
