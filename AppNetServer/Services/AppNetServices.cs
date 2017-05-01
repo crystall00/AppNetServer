1df36676e78be9f41e5bfd206368a11daac83f73
Anton Markaj <thony_m89@hotmail.com>
2017-05-01 19:55:13 +0200

    added userid column to insert command

--- AppNetServer/Services/AppNetServices.cs
+++ AppNetServer/Services/AppNetServices.cs
@@ -17,7 +17,7 @@ namespace AppNetServer.Services
         {
             conn.Open();
             try { 
-                SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Auftrag (erstelldatum,titel,beschreibung,ort) VALUES ('" + auftrag.erstelldatum + "','" + auftrag.titel + "','" + auftrag.beschreibung + "','" + auftrag.ort + "','" + userid + "') ",conn);
+                SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Auftrag (erstelldatum,titel,beschreibung,ort,userid) VALUES ('" + auftrag.erstelldatum + "','" + auftrag.titel + "','" + auftrag.beschreibung + "','" + auftrag.ort + "','" + userid + "') ",conn);
                 cmd.ExecuteNonQuery();
                 conn.Close();
             }
